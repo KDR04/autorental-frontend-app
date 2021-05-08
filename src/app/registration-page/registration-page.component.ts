3fbfd7eac39c0bcf443cfa9ee7d950bd896a6adf
KDR04 <kaushik.dt2@gmail.com>
2021-05-08 21:49:25 +0530

    added validators in the login and register form

--- src/app/registration-page/registration-page.component.ts
+++ src/app/registration-page/registration-page.component.ts
@@ -9,26 +9,57 @@ import { Validators,FormBuilder, FormGroup, FormControl } from '@angular/forms';
 export class RegistrationPageComponent implements OnInit {
 
 
-  registerForm = new FormGroup({
-    firstName: new FormControl('', Validators.required),
-    lastName: new FormControl('', Validators.required),
-    password: new FormControl('', Validators.required),
-    age: new FormControl('', Validators.required),
-    email: new FormControl('', Validators.required),
-    drivingLicense: new FormControl('', Validators.required)
-  })
+  registerForm!: FormGroup
 
-
-  getErrorMessage() {
-    if (this.registerForm.hasError('required')) {
-      return 'You must enter a value';
-    }
-  }
-
-
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      firstName: new FormControl('', Validators.required),
+        lastName: new FormControl('', Validators.required),
+        password: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(8)]),
+        age: new FormControl('', [Validators.required, Validators.pattern('\\-?\\d*\\.?\\d{1,2}')]),
+        email: new FormControl('', [Validators.required, Validators.email]),
+        drivingLicense: new FormControl('', Validators.required)
+    })
   }
+
+
+  getErrorMessageEmail() {
+    if (this.registerForm.get('email').hasError('required')) {
+      return 'You must enter a value';
+    } 
+    return this.registerForm.get('email').hasError('email') ? 'Not a valid email' : '';
+  }
+ getErrorMessagePassword(){
+    
+    // if(this.registerForm.get('password').value > 8 ){
+    //   return 'Length of value must be between 6 to 8';
+    // }else{
+      if(this.registerForm.get('password').hasError('required')){
+        return 'You must enter a value. Size should be between 6 to 8';
+      }
+    // }
+  }
+  getErrorMessageAge(){
+
+      if(this.registerForm.get('age').value < 18){
+        return 'You must enter value of age more than 18.'
+      }
+      return this.registerForm.get('age').hasError('required') ? 'You must enter a value.' : '';
+  }
+
+
+getErrorMessage() {
+  if (this.registerForm.get('firstName').hasError('required')) {
+    return 'You must enter a value';
+  } 
+  if (this.registerForm.get('lastName').hasError('required')) {
+    return 'You must enter a value';
+  } 
+  if(this.registerForm.get('drivingLicense').hasError('required')){
+    return 'You must enter a value';
+  } 
+}
 
 }
